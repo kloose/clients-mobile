306f7193f13fdf76bb71db870117747936fb6e35
Keith <keith@nomadcml.com>
2025-09-25 13:46:33 +0100

    Added server api

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export const AUTH0_DOMAIN = 'dev-ffuy7gqrimo4flu2.uk.auth0.com';
 export const AUTH0_CLIENT_ID = 'oagA9k8icEHEZvRic9mQpQiiPlbydJTI';
-export const API_URL = 'http://localhost:8081/api';
+export const API_URL = 'https://api.rollsandrehab.online/api';
 
 export const AUTH0_CONFIG = {
   domain: AUTH0_DOMAIN,
